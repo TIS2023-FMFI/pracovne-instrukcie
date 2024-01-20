44c96e071c8873b42505014d68bdc6fb6983a35c
Matej Zelenak <maejzele@gmail.com>
2024-01-20 18:44:54 +0100

    move screen resolution to config file, format code

--- src/app/main.py
+++ src/app/main.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QListWidgetItem
 from PyQt5.QtCore import Qt
 from pdf_viewer import PDFViewer
 
+
 class LoginWindow(QDialog):
     def __init__(self) -> None:
         super(LoginWindow, self).__init__()
@@ -19,44 +20,46 @@ class LoginWindow(QDialog):
         self.hide()
 
 
-class MainWindow( QMainWindow ):
+class MainWindow(QMainWindow):
     def __init__(self, login_window) -> None:
         self.login_window: QDialog = login_window
-        super( MainWindow, self ).__init__()
+        super(MainWindow, self).__init__()
         loadUi('ui/main_window.ui', self)
-        self.logout_button.clicked.connect( self.log_out )
+        self.logout_button.clicked.connect(self.log_out)
 
-        self.instructions_dir: str = "../../resources/pdf/"
-        self.instructions: list[ str ] = [ self.instructions_dir + instruction for instruction in os.listdir( self.instructions_dir ) if instruction.endswith( ".pdf" ) ]
-        self.instruction_names: str = [ instruction.removesuffix( ".pdf" )  for instruction in os.listdir( self.instructions_dir ) if instruction.endswith( ".pdf" ) ]
-        self.pdf_viewer: PDFViewer = PDFViewer( self )
+        self.instructions_dir: str = '../../resources/pdf/'
+
+        self.instructions: list[str] = [
+            self.instructions_dir + instruction
+            for instruction in os.listdir(self.instructions_dir) if instruction.endswith('.pdf')
+        ]
+        self.instruction_names: list[str] = [
+            instruction.removesuffix('.pdf')
+            for instruction in os.listdir(self.instructions_dir) if instruction.endswith('.pdf')
+        ]
+        self.pdf_viewer: PDFViewer = PDFViewer(self)
         self.display_instructions()
-        
-        
 
-    def display_instructions( self ):
-        self.listWidget.itemClicked.connect( self.clicked )
-        for name in self.instruction_names: 
-            item = QListWidgetItem( name )
-            item.setTextAlignment( Qt.AlignLeft )
-            self.listWidget.addItem( item )
+    def display_instructions(self):
+        self.listWidget.itemClicked.connect(self.clicked)
+        for name in self.instruction_names:
+            item = QListWidgetItem(name)
+            item.setTextAlignment(Qt.AlignLeft)
+            self.listWidget.addItem(item)
 
-    def log_out( self ) -> None:
+    def log_out(self) -> None:
         self.hide()
         self.pdf_viewer.hide()
         self.login_window.showFullScreen()
-        
-    def clicked( self, item ) -> None:
-        self.open_instruction( item.text() )
 
-    def open_instruction( self, file_name ) -> None:
-        #get display name from file name dict
+    def clicked(self, item) -> None:
+        self.open_instruction(item.text())
+
+    def open_instruction(self, file_name) -> None:
+        # get display name from file name dict
         name: str = file_name
-        self.pdf_viewer.set_document( self.instructions_dir + file_name + '.pdf', name )
+        self.pdf_viewer.set_document(self.instructions_dir + file_name + '.pdf', name)
         self.pdf_viewer.display()
-    
-
-
 
 
 if __name__ == '__main__':
