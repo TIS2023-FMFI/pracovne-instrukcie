eb2aac422897bc55bb6eb7da02d25d26cf5ded41
MatejBudos <budos.matej10@gmail.com>
2024-02-02 00:06:22 +0100

    implement add_instruction

--- src/app/instruction_manager.py
+++ src/app/instruction_manager.py
@@ -9,6 +9,9 @@ from PyQt5 import QtWidgets
 
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QHBoxLayout, QLabel, QPushButton, \
     QToolButton, QSpacerItem
+from PyQt5.QtCore import QDate
+import datetime
+from dateutil.relativedelta import relativedelta
 
 from confirmation_window import ConfirmationWindow
 from database_manager import DBManager
@@ -24,10 +27,17 @@ class InstructionManager( QWidget ):
         self.database = DBManager()
         self.confirmation_window = ConfirmationWindow()
         self.confirmation_window.signal.connect( self.delete_instruction )
+        self.select_button.clicked.connect( self.select_file )
+        self.add_button.clicked.connect(self.add_instruction)
+        today = datetime.datetime.today()
+        self.validation_date.setDate( QDate.currentDate() )
         self.instruction_id = None
 
 
-
+    def select_file(self) -> None:
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select file", "../../resources/pdf", "PDF files (*.pdf)")
+        file_name = path.split("/")[-1]
+        self.path_label.setText(file_name)
 
 
     def delete_instruction( self ):
@@ -36,7 +46,21 @@ class InstructionManager( QWidget ):
         self.instruction_id = None
 
     def add_instruction( self ):
-        ...
+        name: str = self.instruction_name.text()
+        path: str = self.path_label.text()
+        validation_date: QDate = self.validation_date.date()
+        frequency: int = int(self.frequency_combobox.currentText())
+        if path == "" or name == "":
+            return
+        validation_date: str = validation_date.toString('yyyy-MM-dd')
+        validation_date_converted: datetime = datetime.datetime.strptime( validation_date, '%Y-%m-%d' )
+        expiration_date = validation_date_converted + relativedelta( months=+frequency )
+      
+        query = f" insert into instructions ( name, file_path, validation_date, expiration_date )\
+        values('{name}','{path}','{validation_date}','{expiration_date.date()}')"
+        self.database.execute_query( query )
+        self.hide()
+
 
 
     def confirmation( self, instruction_id ):
