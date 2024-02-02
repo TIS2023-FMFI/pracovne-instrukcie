332d78aff61b78f56938e6e9dd6719d70828fc5a
Matej Zelenak <maejzele@gmail.com>
2024-02-02 23:28:50 +0100

    fix keyword search with DB query and file path

--- src/app/keyword_search.py
+++ src/app/keyword_search.py
@@ -5,24 +5,27 @@ from unidecode import unidecode
 from constants import INSTRUCTIONS_DIR
 from instruction import Instruction
 
+from database_manager import DBManager
 
 class Search:
     def __init__(self) -> None:
         self.words_in_pdf: dict[str, set[str]] = dict()
+        self.database: DBManager = DBManager()
         # TODO: read PDFs at startup
-        # self.preread_pdfs()
+        self.preread_pdfs()
 
     def preread_pdfs(self) -> None:
-        for instruction in os.listdir(INSTRUCTIONS_DIR):
-            if instruction.endswith('.pdf'):
-                self.read_pdf(instruction.removesuffix('.pdf'))
+        instructions_list_path = [instr[0] for instr in self.database.execute_query(f"SELECT file_path FROM instructions")]
+        for instruction_path in instructions_list_path:
+            if instruction_path.endswith('.pdf') and os.path.exists(instruction_path):
+                self.read_pdf(instruction_path)
 
-    def read_pdf(self, instruction: str) -> None:
+    def read_pdf(self, instruction_path: str) -> None:
         set_of_words: set[str] = set()
-        for word in instruction.split():
+        for word in instruction_path.split():
             set_of_words.add(unidecode(word.lower()))
 
-        pdfFileObj = open(INSTRUCTIONS_DIR + instruction + '.pdf', 'rb')
+        pdfFileObj = open(instruction_path, 'rb')
         pdfReader = PyPDF2.PdfReader(pdfFileObj)
         for i in range(len(pdfReader.pages)):
             pageObj = pdfReader.pages[i]
@@ -32,16 +35,16 @@ class Search:
 
         pdfFileObj.close()
 
-        self.words_in_pdf[instruction] = set_of_words
+        self.words_in_pdf[instruction_path] = set_of_words
 
-    def contains_keyword(self, instruction: str, keyword: str = '') -> bool:
+    def contains_keyword(self, path: str, keyword: str = '') -> bool:
         if keyword == '':
             return True
 
-        if instruction not in self.words_in_pdf:
-            self.read_pdf(instruction)
+        if path not in self.words_in_pdf:
+            self.read_pdf(path)
 
-        for word in self.words_in_pdf[instruction]:
+        for word in self.words_in_pdf[path]:
             if word.startswith(keyword):
                 return True
 
@@ -50,7 +53,7 @@ class Search:
     def filter_instructions(self, keyword: str, history: tuple[int], all_instructions: list[Instruction]) -> list[Instruction]:
         instructions: list[Instruction] = list()
         for _id in all_instructions:
-            if self.contains_keyword(_id.name, unidecode(keyword.lower())):
+            if self.contains_keyword(_id.file_path, unidecode(keyword.lower())):
                 instructions.append(_id)
 
         instruction_ids: dict[int, Instruction] = {instr.id: instr for instr in instructions}
