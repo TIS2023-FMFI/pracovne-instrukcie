c7f33f39cbf30625d7b651d38179ec4e68e90424
Matej Zelenak <maejzele@gmail.com>
2024-02-04 18:46:36 +0100

    rename main css

--- src/app/main.py
+++ src/app/main.py
@@ -210,6 +210,6 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyleSheet(open('ui/login.css').read())
+    app.setStyleSheet(open('ui/main.css').read())
     w = MainWindow()
     app.exec()
