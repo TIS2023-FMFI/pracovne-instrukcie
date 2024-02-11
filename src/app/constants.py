ce0667267a05814941d3435b16103536f3b9c092
Matej Zelenak <maejzele@gmail.com>
2024-02-11 18:17:26 +0100

    remove code comments

--- src/app/constants.py
+++ src/app/constants.py
@@ -14,9 +14,9 @@ def show_notice(self, message: str) -> None:
     warning_label.setStyleSheet('font-size: 14pt;')
     warning_layout.addWidget(warning_label)
     warning_dialog.setLayout(warning_layout)
-    # Set up a QTimer to close the dialog after 2 seconds
+
     timer = QTimer(warning_dialog)
-    timer.timeout.connect(warning_dialog.accept)  # Close the dialog
-    timer.start(2500)  # 2000 milliseconds = 2 seconds
+    timer.timeout.connect(warning_dialog.accept)
+    timer.start(2500)
 
     warning_dialog.exec_()
