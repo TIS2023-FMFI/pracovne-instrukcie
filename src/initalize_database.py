6fdeeead7e485c7e340326ddd318797877fe323d
Matej Zelenak <maejzele@gmail.com>
2024-02-12 15:29:46 +0100

    fix initializing database with same local instructions

--- src/initalize_database.py
+++ src/initalize_database.py
@@ -8,11 +8,14 @@ valid_until = '2024-09-01'
 db = DBManager()
 db.run_script('initialize_database.sql')
 
+all_paths = [path[0] for path in db.execute_query("SELECT file_path FROM instructions")]
+print(*all_paths, sep='\n')
+
 for root, dirs, files in os.walk(INSTRUCTIONS_DIR):
     for file in files:
         if file.endswith('.pdf'):
             file_path = os.path.join(root, file)
-
-            instruction = (file.removesuffix('.pdf'), file_path, valid_until)
-            db.execute_query(f"INSERT INTO instructions (name, file_path, expiration_date) "
-                             f"VALUES {instruction}; ")
+            if file_path not in all_paths:
+                instruction = (file.removesuffix('.pdf'), file_path, valid_until)
+                db.execute_query(f"INSERT INTO instructions (name, file_path, expiration_date) "
+                                 f"VALUES {instruction}; ")
