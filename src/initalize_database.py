a4461da33184430ed59311ae71730d02b31c1552
Matej Zelenak <maejzele@gmail.com>
2024-02-12 15:20:37 +0100

    init db: create variable valid until

--- src/initalize_database.py
+++ src/initalize_database.py
@@ -3,6 +3,8 @@ import os
 from constants import INSTRUCTIONS_DIR
 from database_manager import DBManager
 
+valid_until = '2024-09-01'
+
 db = DBManager()
 db.run_script('initialize_database.sql')
 
@@ -11,6 +13,6 @@ for root, dirs, files in os.walk(INSTRUCTIONS_DIR):
         if file.endswith('.pdf'):
             file_path = os.path.join(root, file)
 
-            instruction = (file.removesuffix('.pdf'), file_path, '2024-07-25')
+            instruction = (file.removesuffix('.pdf'), file_path, valid_until)
             db.execute_query(f"INSERT INTO instructions (name, file_path, expiration_date) "
                              f"VALUES {instruction}; ")
